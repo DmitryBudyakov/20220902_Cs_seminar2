15def42f581a4c6b0498eaa2e0590e4266d89e04
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-02 20:23:33 +0300

    Task09 первая версия

---
+++ Task09/Program.cs
@@ -0,0 +1,14 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+
+int firstDigit = number / 10;   // находим 1-ю цифру числа
+int secondDigit = number % 10;   // находим 2-ю цифру числа
+
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
