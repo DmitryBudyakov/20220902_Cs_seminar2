92cda4e8e038cfe48b89ea48b3e119cbf3f430e2
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-02 20:32:42 +0300

    Task09 - с тернарным опреатором и msg о равенстве

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,9 +6,18 @@
 // 85 -> 8
 
 int number = new Random().Next(10, 100);
+Console.WriteLine(number);
 
 int firstDigit = number / 10;   // находим 1-ю цифру числа
 int secondDigit = number % 10;   // находим 2-ю цифру числа
 
-if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
+
+// через тернарный оператор
+if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+else
+{
+    int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+    Console.WriteLine($"Наибольшая цифра числа {number} -> {max}");
+}
